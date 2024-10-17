1ef68ac3a0048f28d3af7f551a0991a86f07711f
LapusanVlad <Lapusan.Co.Vlad@student.utcluj.ro>
2024-10-17 11:45:31 +0300

    Add files via upload

---
+++ LapusanVlad/tema2.py
@@ -0,0 +1,6 @@
+print("""Eurodeputata Diana Șoșoacă Își Depune, Joi, Candidatura
+Pentru alegerile parlamentare pentru un mandat de senatoare""")
+
+a = """Eurodeputata Diana Șoșoacă Își Depune, Joi, Candidatura"""
+b = """Pentru alegerile parlamentare pentru un mandat de senatoare"""
+print(a + b)
