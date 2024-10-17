92a68f2086e22c51b36a34eebd134a297f76c515
LapusanVlad <Lapusan.Co.Vlad@student.utcluj.ro>
2024-10-17 20:27:32 +0300

    Delete LapusanVlad/tema2.py

--- LapusanVlad/tema2.py
+++
@@ -1,6 +0,0 @@
-print("""Eurodeputata Diana Șoșoacă Își Depune, Joi, Candidatura
-Pentru alegerile parlamentare pentru un mandat de senatoare""")
-
-a = """Eurodeputata Diana Șoșoacă Își Depune, Joi, Candidatura"""
-b = """Pentru alegerile parlamentare pentru un mandat de senatoare"""
-print(a + b)
