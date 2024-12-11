2701f739dc8f0bb0e1de0c2e339539fece6309ab
LapusanVlad <Lapusan.Co.Vlad@student.utcluj.ro>
2024-12-11 14:43:26 +0200

    Add files via upload

---
+++ Tema7.py
@@ -0,0 +1,22 @@
+import time
+import os
+
+
+def bubble_sort_visualize(arr):
+    n = len(arr)
+    pasul = 1
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+            print(f"Pasul {pasul}: {arr}")
+            pasul += 1
+            time.sleep(1.5)
+
+    print("\nLista sortata: ", arr)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort_visualize(arr)
+
